b247bf7ab3a18ebb66c554f7bda4ae2cb9d9068a
EdyPEN <edy2.0andre@gmail.com>
2025-11-26 08:19:37 +0000

    Forby 1 bullet rotation

--- Assets/Scripts/Enemies/ForbyBehaviour.cs
+++ Assets/Scripts/Enemies/ForbyBehaviour.cs
@@ -18,41 +18,110 @@ public class ForbyBehaviour : MonoBehaviour
     public float shootingCooldownTimer;
     public float shootingSpeed;
 
+    public Vector2 shootingPositionOffset;
+    public Vector2 shootingPosition;
+
+    public int maxNumberOfShots = 4;
+    public int currentNumberOfShots;
+
+    public float rotationSpeed;
+    public int numberOfRotationsPerShot;
+    public int rotationState;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        shootingPosition = new Vector2(transform.position.x + shootingPositionOffset.x, transform.position.y + shootingPositionOffset.y);
         shootingCooldownTimer = shootingCooldown;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Vector2.Distance(player.transform.position, transform.position) <= range)
+        //Rotating
+        if (rotationState == 0)
         {
-                shootingCooldownTimer -= Time.fixedDeltaTime;
-
-            if (shootingCooldownTimer <= 0 && dead == false)
+            bulletArray[0].SetActive(true);
+            bulletArray[0].transform.position = shootingPosition;
+            rotationState = 1;
+        }
+        else if (rotationState == 1)
+        {
+            bulletArray[0].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            if (bulletArray[0].transform.rotation.z <= -90 && bulletArray[0].transform.rotation.z > -180)
             {
-                shootingCooldownTimer = shootingCooldown;
-
-                Vector2 shootingDirectionalForce = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
-
-                float shootingMagnitude = Mathf.Sqrt(Mathf.Pow(shootingDirectionalForce.x, 2) + Mathf.Pow(shootingDirectionalForce.y, 2));
-
-
-                for (int i = 0; i < bulletArray.Length; i++)
-                {
-                    if (bulletArray[i].activeSelf == false)
-                    {
-                        bulletArray[i].transform.position = transform.position;
-                        bulletArray[i].SetActive(true);
-                        bulletArray[i].GetComponent<Rigidbody2D>().linearVelocityX = (shootingDirectionalForce.x / shootingMagnitude) * shootingSpeed;
-                        bulletArray[i].GetComponent<Rigidbody2D>().linearVelocityY = (shootingDirectionalForce.y / shootingMagnitude) * shootingSpeed;
-                        return;
-                    }
-                }
+                bulletArray[1].SetActive(true);
+                bulletArray[1].transform.position = shootingPosition;
+                rotationState = 2;
             }
         }
+        else if (rotationState == 2)
+        {
+            bulletArray[0].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            bulletArray[1].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            if (bulletArray[0].transform.rotation.z <= 180 && bulletArray[0].transform.rotation.z > 90)
+            {
+                bulletArray[2].SetActive(true);
+                bulletArray[2].transform.position = shootingPosition;
+                rotationState = 3;
+            }
+        }
+        else if (rotationState == 3)
+        {
+            bulletArray[0].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            bulletArray[1].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            bulletArray[2].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            if (bulletArray[0].transform.rotation.z <= 90 && bulletArray[0].transform.rotation.z > 0)
+            {
+                bulletArray[3].SetActive(true);
+                bulletArray[3].transform.position = shootingPosition;
+                rotationState = 4;
+            }
+        }
+        else if (rotationState == 4)
+        {
+            bulletArray[0].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            bulletArray[1].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            bulletArray[2].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            bulletArray[3].transform.RotateAround(transform.position, Vector3.forward, -rotationSpeed * Time.fixedDeltaTime);
+            if (bulletArray[0].transform.rotation.z <= 0 && bulletArray[0].transform.rotation.z > -90)
+            {
+                //rotationState = 5;
+            }
+        }
+
+
+        //Shooting
+        //if (currentNumberOfShots < maxNumberOfShots)
+        //{
+        //    if (Vector2.Distance(player.transform.position, transform.position) <= range)
+        //    {
+        //            shootingCooldownTimer -= Time.fixedDeltaTime;
+
+        //        if (shootingCooldownTimer <= 0 && dead == false)
+        //        {
+        //            shootingCooldownTimer = shootingCooldown;
+
+        //            Vector2 shootingDirectionalForce = new Vector2(player.transform.position.x - shootingPosition.x, player.transform.position.y - shootingPosition.y);
+
+        //            float shootingMagnitude = Mathf.Sqrt(Mathf.Pow(shootingDirectionalForce.x, 2) + Mathf.Pow(shootingDirectionalForce.y, 2));
+
+
+        //            for (int i = 0; i < bulletArray.Length; i++)
+        //            {
+        //                if (bulletArray[i].activeSelf == false)
+        //                {
+        //                    bulletArray[i].transform.position = shootingPosition;
+        //                    //bulletArray[i].SetActive(true);
+        //                    bulletArray[i].GetComponent<Rigidbody2D>().linearVelocityX = (shootingDirectionalForce.x / shootingMagnitude) * shootingSpeed;
+        //                    bulletArray[i].GetComponent<Rigidbody2D>().linearVelocityY = (shootingDirectionalForce.y / shootingMagnitude) * shootingSpeed;
+        //                    currentNumberOfShots++;
+        //                    return;
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
